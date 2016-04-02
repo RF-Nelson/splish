4e691ab44004916f0d9f4970730c90402f7aed0e
Richard Nelson <richardnelson@Richards-Mac-mini.local>
2016-04-02 12:40:46 -0400

    create db seeds

--- splish-webapp/db/seeds.rb
+++ splish-webapp/db/seeds.rb
@@ -1,3 +1,5 @@
+import 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+50.times do
+  Guest.create!([{
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    password_digest: "needTextToPassValidation"
+  }])
+end
+
+50.times do
+  started_already = [true, false].sample
+  sample_array = (0..300).to_a
+
+  if started_already
+    start_date = sample_array.sample.days.ago
+  else
+    start_date = sample_array.sample.days.from_now
+  end
+  # Create an array that will be sampled. The value returned will
+  # be the length of the event; I've weighted the odds so more
+  # short events are seeded than long events.
+  event_length = [1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,3,4,5,6,7].sample
+  end_date = start_date + event_length.days
+
+  Event.create([{
+    owner_id: (0..30).to_a.sample,
+    title: Faker::Hipster.sentence(3, true, 2),
+    description: Faker::Hipster.paragraph(2, true, 2),
+    start_date: start_date,
+    end_date: end_date,
+    location: Faker::Address.city + ', ' + Faker::Address.state
+  }])
+end
