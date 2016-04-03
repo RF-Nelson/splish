4c9f68b8ba3cf9cd9bba880e8f9462eab2b5d0c2
Richard Nelson <richardnelson@Richards-Mac-mini.local>
2016-04-03 16:19:16 -0400

    add animated logo

--- splish-webapp/app/assets/javascripts/application.js
+++ splish-webapp/app/assets/javascripts/application.js
@@ -10,9 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require ./vendor/mo.min.js
+//= require ./vendor/segment.min.js
+//= require ./vendor/d3-ease.v0.6.js
+//= require ./vendor/letters.js
 //= require jquery
 //= require jquery_ujs
 //= require angular/angular
 //= require ng-rails-csrf
+//= require ./logo.js
 //= require_self
 //= require_tree .
