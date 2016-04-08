52547f4d9bc7301b0f1b0be890bfdaaef83ce24c
Rich Nelson <rnelson@alumni.upenn.edu>
2016-04-08 12:31:18 -0400

    remove commented HTML that was being parsed

--- splish-webapp/db/seeds.rb
+++ splish-webapp/db/seeds.rb
@@ -47,7 +47,7 @@ end
   Event.create([{
     owner_id: (0..30).to_a.sample,
     title: Faker::Hipster.sentence(3, true, 2),
-    description: Faker::Hipster.paragraph(2, true),
+    description: Faker::Hipster.paragraph(1, true),
     start_date: start_date,
     end_date: end_date,
     location: Faker::Address.city + ', ' + Faker::Address.state
